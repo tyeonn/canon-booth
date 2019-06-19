ff3e641ec4de1cccd997f03d72dd6ce738315661
John Kim <jkim82092@gmail.com>
2019-06-19 19:14:48 -0400

    Remove modal info

--- src/settings.jsx
+++ src/settings.jsx
@@ -9,14 +9,6 @@ class Settings extends React.Component {
         // };
     }
 
-    // showModal() {
-    //     this.setState({ show: true });
-    // }
-
-    // hideModal() {
-    //     this.setState({ show: false });
-    // }
-
     render() {
         return (
             <div className="settings-page">
@@ -24,7 +16,7 @@ class Settings extends React.Component {
                     <h2 className="settings-title">Lifestyle</h2>
                     <p className="settings-text">Adjust the filters and see how you look on the photobooth screen! Once you're satisfied, just take a picture using the shutter button below.</p>
                 </div>
-                <div className="">
+                <div className="settings-options">
                     <div className="settings-left-side">
                         <p>Saturation</p>
                         <p>Highlights</p>
@@ -32,19 +24,13 @@ class Settings extends React.Component {
                         <p>Aperature</p>
                         <p>Brightness</p>
                         <p>Contrast</p>
-                        <p>Brightness</p>
+                        <p>Shutter&nbsp;Speed</p>
                     </div>
                     <div className="settings-right-side">
-                        <div className="settings-toggle-bar">
-
-                        </div>
+                        <div className="settings-toggle-bar"></div>
+                        <div className="settings-toggle-bar"></div>
                     </div>
                 </div>
-
-                {/* <Modal show={this.state.show} handleClose={this.hideModal} >
-                    <p>Modal</p>
-                </Modal>
-                <button type='button' onClick={this.showModal}>Open</button> */}
             </div>
         )
     }
