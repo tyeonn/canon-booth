12084a8cf6c966e9b467344751ce237e0cb98d9d
Timothy Wu <timothyjwu1@gmail.com>
2019-06-20 03:36:18 -0400

    alert msg

--- src/End.js
+++ src/End.js
@@ -1,6 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
+import Swal from'sweetalert2';
 
+const handleEnd = (hide) => {
+    // hide();
+    setTimeout(() => {
+        Swal.fire({
+          type: 'success',
+          title: 'Thanks for using Canon Booth!',
+          timer: 1500,
+          showConfirmButton: false,
+        });
+  
+    },600);
+
+};
 const End = ({ isShowing, hide }) => isShowing ? (
     <div>
         <div onClick={hide} className="modal-overlay"/>
@@ -8,7 +23,7 @@ const End = ({ isShowing, hide }) => isShowing ? (
             <h1 className="end-modal-text">Are you sure you want to end your session?</h1>
             <div className="modal-buttons">
                 <button onClick={hide} id="cancel" className="modal-button">Cancel</button>
-                <button onClick={hide} id="end" className="modal-button">End</button>
+                <Link to='/' onClick={handleEnd} id="end" className="modal-button">End</Link>
             </div>
         </div>
     </div>
