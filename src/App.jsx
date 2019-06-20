77805f8e830de52060162ab472e80d99fc836025
Dante Cantal <danteacantal@gmail.com>
2019-06-19 20:12:25 -0400

    preset index and index item

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './stylesheets/App.css';
 import { Route, Link, Switch } from 'react-router-dom';
-import Splash from './splash';
+// import Splash from './splash';
+import PresetIndex from './PresetIndex';
+import PhoneNav from './PhoneNav';
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path='/' component={Splash}></Route>
+        {/* <Route path='/' component={Splash}></Route> */}
+        {/* <Route path='/' component={PhoneNav}></Route> */}
+        <Route path='/phone' component={PresetIndex}></Route>
       </Switch>
     </div>
   );
