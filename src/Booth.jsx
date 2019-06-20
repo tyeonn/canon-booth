3555b3a2b1d4e31e8bff8da756d664542eba6482
Timothy Wu <timothyjwu1@gmail.com>
2019-06-20 11:25:47 -0400

    liveview and autofocus on camera

--- src/Booth.jsx
+++ src/Booth.jsx
@@ -1,12 +1,39 @@
-import React from 'react';
+import React from "react";
 
-function Booth(){
-    return <div>
-        <p>
-            <video id="live-booth-view" autoPlay="autoPlay" src="http://localhost:8080">
-            </video>
-        </p>
-    </div>
+class Booth extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    componentDidMount() {
+        fetch("http://192.168.1.2:8080/ccapi/ver100/shooting/liveview", {
+            method: "POST",
+            body: JSON.stringify({
+            liveviewsize: "small",
+            cameradisplay: "on"
+            })
+        });
+        fetch("http://192.168.1.2:8080/ccapi/ver100/shooting/control/af", {
+            method: "POST",
+            body: JSON.stringify({
+                action: "start"
+            })
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <p>
+                <video
+                    id="live-booth-view"
+                    autoPlay="autoPlay"
+                    src="http://localhost:8080"
+                />
+                </p>
+            </div>
+        );
+    }
 }
 
 export default Booth;
